864c4dbbe9aff18803154fdd1a5b266aed59039c
Rajesh Arumugam <rajesh.x.arumugam.consultant@nielsen.com>
2021-06-28 10:38:05 +0530

    final code

--- Tree/VehicleRecord.py
+++ Tree/VehicleRecord.py
@@ -1,179 +1,100 @@
 import sys
 
-
-class TruckNode:
+class Node(object):
     def __init__(self, Uid):
         self.UId = Uid
-        self.chkoutCtr = 0
         self.left = None
         self.right = None
-        self.height = 1
+        self.height = 0
+        self.chkoutCtr = 0
 
 
-class AVLTree:
+class AVLTree(object):
     def __init__(self):
         self.root = None
         self.max_count = None
         self.count = 0
 
-    def countTrucks(self, root):
-        """
-        helper function to count the total trucks
-        :param root:
-        :return: count
-        """
-        if root is None:
+    def height(self, node):
+        if node is None:
             return -1
         else:
-            count = 1
-            if root.left is not None:
-                count += self.countTrucks(root.left)
-            if root.right is not None:
-                count += self.countTrucks(root.right)
-            return count
+            return node.height
 
-    def inorder(self, root):
-        if root is not None:
-            self.inorder(root.left)
-            print(root.UId, ", ", root.chkoutCtr)
-            self.inorder(root.right)
+    # left left
+    def singleLeftRotate(self, node):
+        k1 = node.left
+        node.left = k1.right
+        k1.right = node
+        node.height = max(self.height(node.right), self.height(node.left)) + 1
+        k1.height = max(self.height(k1.left), node.height) + 1
+        return k1
 
-    def searchTree(self, root, key):
-        """
-        binary search function
-        :param root: root node of the tree
-        :param key: key to be searched in the tree
-        :return: Truck ID, status, chkoutCtr
-        """
-        if root.UId == key:
-            return root.UId, 2, root.chkoutCtr
-        if root.UId < key:
-            if root.right is None:
-                return key, -1, root.chkoutCtr
-            return self.searchTree(root.right, key)
-        if root.UId > key:
-            if root.left is None:
-                return key, -1, root.chkoutCtr
-            return self.searchTree(root.left, key)
+    # right right
+    def singleRightRotate(self, node):
+        k1 = node.right
+        node.right = k1.left
+        k1.left = node
+        node.height = max(self.height(node.right), self.height(node.left)) + 1
+        k1.height = max(self.height(k1.right), node.height) + 1
+        return k1
 
-    def getList(self, root):
-        if not root:
-            return
-        yield from self.getList(root.left)
-        yield root.UId, root.chkoutCtr
-        yield from self.getList(root.right)
+    # right left
+    def doubleLeftRotate(self, node):
+        node.left = self.singleRightRotate(node.left)
+        return self.singleLeftRotate(node)
 
-    def searchTreeRecord(self, root):
-        # print(printCount)
-        if root is None:
-            return
-        else:
-            self.searchTreeRecord(root.left)
-            if (root.chkoutCtr % 2 == 0 and root.chkoutCtr < 2 * self.max_count) or root.chkoutCtr == 0:
-                print(root.UId)
-            self.searchTreeRecord(root.right)
+    # left right
+    def doubleRightRotate(self, node):
+        node.right = self.singleLeftRotate(node.right)
+        return self.singleRightRotate(node)
 
-    def countAvailTruck(self, root):
-        try:
-            if root is None:
-                return 0
-            else:
-                self.countAvailTruck(root.left)
-                if (root.chkoutCtr % 2 == 0 and root.chkoutCtr < 2 * self.max_count) or root.chkoutCtr == 0:
-                    self.count += 1
-                self.countAvailTruck(root.right)
-        except Exception as e:
-            print(str(e))
-
-    def getHeight(self, current):
-        if current is None:
-            return 0
-        else:
-            return current.height
-
-    def getBalance(self, current):
-        if current is None:
-            return 0
-        else:
-            return self.getHeight(current.left) - self.getHeight(current.right)
-
-    def rotateRight(self, current):
-        y = current.left
-        temp = y.right
-
-        y.right = current
-        current.left = temp
-
-        current.height = max(self.getHeight(current.left), self.getHeight(current.right)) + 1
-        y.height = max(self.getHeight(y.left), self.getHeight(y.right)) + 1
-        return y
-
-    def rotateLeft(self, current):
-        y = current.right
-        temp = y.left
-        y.left = current
-        current.right = temp
-        current.height = max(self.getHeight(current.left), self.getHeight(current.right)) + 1
-        y.height = max(self.getHeight(y.left), self.getHeight(y.right)) + 1
-        return y
-
-    def triggerReadTruckRec(self, value):
-        """
-        trigger function for _readTruckRec
-        :param value: truck_id from inputPS2.txt
-        :return: assign max delivery value
-        """
-        if self.root is None:
+    def triggerReadTruckRec(self, key):
+        if not self.root:
             if self.max_count is None:
-                self.max_count = value
-                # print('Assigning the maximum deliveries :', value)
+                self.max_count = key
             else:
-                self.root = TruckNode(value)
-                # print('setting root node - {}, counter - {}'.format(value, self.root.chkoutCtr))
+                self.root = Node(key)
         else:
-            self._readTruckRec(self.root, value)
+            self.root = self._readTruckRec(self.root, key)
 
     def _readTruckRec(self, tNode, Uid):
-        """
-        create tree from the truck_ids, update chkoutCtr
-        :param tNode: root node of the tree
-        :param Uid: truck_id to be inserted
-        :return: increment chkoutCtr for every subsequent occurrence of that truck
-        """
-        if Uid < tNode.UId:
-            if tNode.left is None:
-                tNode.left = TruckNode(Uid)
-            else:
-                self._readTruckRec(tNode.left, Uid)
-            # print('less than root node - {}, counter - {}'.format(tNode.UId, tNode.chkoutCtr))
+        if tNode is None:
+            tNode = Node(Uid)
+        elif Uid < tNode.UId:
+            tNode.left = self._readTruckRec(tNode.left, Uid)
+            if (self.height(tNode.left) - self.height(tNode.right)) == 2:
+                if Uid < tNode.left.UId:
+                    tNode = self.singleLeftRotate(tNode)
+                else:
+                    tNode = self.doubleLeftRotate(tNode)
+
         elif Uid > tNode.UId:
-            if tNode.right is None:
-                tNode.right = TruckNode(Uid)
-            else:
-                self._readTruckRec(tNode.right, Uid)
-            # print('greater than root node - {}, counter - {}'.format(tNode.UId, tNode.chkoutCtr))
+            tNode.right = self._readTruckRec(tNode.right, Uid)
+            if (self.height(tNode.right) - self.height(tNode.left)) == 2:
+                if Uid < tNode.right.UId:
+                    tNode = self.doubleRightRotate(tNode)
+                else:
+                    tNode = self.singleRightRotate(tNode)
         else:
-            # print('Value {} already inserted, counter - {}'.format(Uid, tNode.chkoutCtr))
             tNode.chkoutCtr += 1
             if tNode.chkoutCtr > 2 * self.max_count:
                 print(f'vehicle id {Uid} no longer available for service')
-        tNode.height = max(self.getHeight(tNode.left), self.getHeight(tNode.right)) + 1
-        balance = self.getBalance(tNode)
-        # left left
-        if balance > 1 and Uid < tNode.left.UId:
-            return self.rotateRight(tNode)
-        # right right
-        if balance < -1 and Uid > tNode.right.UId:
-            return self.rotateLeft(tNode)
-        # left right
-        if balance > 1 and Uid > tNode.left.UId:
-            tNode.left = self.rotateLeft(tNode.left)
-            return self.rotateRight(tNode)
-        # right left
-        if balance < -1 and Uid < tNode.right.UId:
-            tNode.right = self.rotateRight(tNode.right)
-            return self.rotateLeft(tNode)
+        tNode.height = max(self.height(tNode.right), self.height(tNode.left)) + 1
         return tNode
+
+    def traverse(self, root):
+        if root is not None:
+            self.traverse(root.left)
+            if root.chkoutCtr > self.max_count:
+                print(root.UId)
+            self.traverse(root.right)
+
+    def inorder(self, node):
+        if node:
+            self.inorder(node.left)
+            print(node.UId, ", ", node.chkoutCtr)
+            self.inorder(node.right)
 
     def triggerUpdateTruckRec(self, prompt):
         """
@@ -199,6 +120,22 @@ class AVLTree:
             print(f'Vehicle Id {Uid} record updated')
             print('------------------------------------')
 
+    def countTrucks(self, root):
+        """
+        helper function to count the total trucks
+        :param root:
+        :return: count
+        """
+        if root is None:
+            return -1
+        else:
+            count = 1
+            if root.left is not None:
+                count += self.countTrucks(root.left)
+            if root.right is not None:
+                count += self.countTrucks(root.right)
+            return count
+
     def _printTruckRec(self, tNode):
         """
         function to count the total number of vehicles that came to the warehouse
@@ -211,11 +148,6 @@ class AVLTree:
         print('------------------------------------')
 
     def triggerCheckTruckRec(self, prompt):
-        """
-        function to read Truck ID and trigger _checkTruckRec
-        :param prompt: checkTruckRec
-        :return: None
-        """
         if prompt is not None:
             split_prompt = prompt.split(':')
             truck_id = int(split_prompt[1].lstrip())
@@ -230,18 +162,40 @@ class AVLTree:
         """
         if type(Uid) == int:
             result = self.searchTree(tNode, Uid)
-            if result[1] == -1:
-                print(f'Vehicle id {result[0]} did not come to the warehouse today')
-            else:
-                if result[1] == 0:
-                    print(f'Vehicle id {result[0]} just reached the warehouse')
-                if result[1] % 2 == 0:
-                    print(f'Vehicle id {result[0]} entered {result[2]} times into the system. '
-                          f'It just completed an order')
-                if result[1] % 2 != 0:
-                    print(f'Vehicle id {result[0]} entered {result[2]} times into the system. '
-                          f'It is currently fulfilling an open order')
-            print('------------------------------------')
+            if result:
+                if result[1] == -1:
+                    print(f'Vehicle id {result[0]} did not come to the warehouse today')
+                else:
+                    if result[1] == 0:
+                        print(f'Vehicle id {result[0]} just reached the warehouse')
+                    if result[1] % 2 == 0:
+                        print(f'Vehicle id {result[0]} entered {result[2]} times into the system. '
+                              f'It just completed an order')
+                    if result[1] % 2 != 0:
+                        print(f'Vehicle id {result[0]} entered {result[2]} times into the system. '
+                              f'It is currently fulfilling an open order')
+                print('------------------------------------')
+
+    def searchTree(self, root, key):
+        """
+        binary search function
+        :param root: root node of the tree
+        :param key: key to be searched in the tree
+        :return: Truck ID, status, chkoutCtr
+        """
+        if root:
+            if root.UId == key:
+                return root.UId, 2, root.chkoutCtr
+            if root.UId < key:
+                if root.right is None:
+                    return key, -1, root.chkoutCtr
+                return self.searchTree(root.right, key)
+            if root.UId > key:
+                if root.left is None:
+                    return key, -1, root.chkoutCtr
+                return self.searchTree(root.left, key)
+        else:
+            return
 
     def triggerPrintOrderStatus(self, prompt):
         """
@@ -280,6 +234,13 @@ class AVLTree:
         print(f'Yet to be fulfilled: {balance}')
         print('------------------------------------')
 
+    def getList(self, root):
+        if not root:
+            return
+        yield from self.getList(root.left)
+        yield root.UId, root.chkoutCtr
+        yield from self.getList(root.right)
+
     def triggerHighFreqTrucks(self, prompt):
         """
         function to read limit and trigger _highFreqTrucks
@@ -300,47 +261,54 @@ class AVLTree:
         :param frequency: frequency limit (z)
         :return: Truck ID, counter
         """
-        tree = list(self.getList(tNode))
+        result = list(self.getList(tNode))
+        tree = [i for i in result if i[1] > frequency]
         if len(tree) == 0:
             print('No such vehicle present in the system')
         else:
             for x in tree:
                 if x[1] > frequency:
-                    # print(x)
                     print(', '.join(map(str, x)))
         print('------------------------------------')
 
-    def tmaxDeliveries(self, prompt):
-        self._maxDeliveries(self.root)
-
-    def traverse(self, root):
-        if root is not None:
-            self.traverse(root.left)
-            if root.chkoutCtr > self.max_count:
-                print(root.UId)
-            self.traverse(root.right)
 
     def _maxDeliveries(self, tNode):
         print(f'maxDeliveries: {self.max_count}')
-        tree = list(self.getList(tNode))
-        count = []
-        for x in tree:
-            if x[1] >= 2 * self.max_count:
-                count.append(x)
-        print(f'{len(count)} Vehicle Ids did their maximum deliveries:')
+        result = list(self.getList(tNode))
+        tree = [i for i in result if i[1] >= 2*self.max_count]
+        print(f'{len(tree)} Vehicle Ids did their maximum deliveries:')
         self.traverse(tNode)
 
         print('------------------------------------')
 
-    # def tavailTrucks(self, prompt):
-    #     self._availTrucks(self.root)
+    def countAvailTruck(self, root):
+        try:
+            if root is None:
+                return 0
+            else:
+                self.countAvailTruck(root.left)
+                if (root.chkoutCtr % 2 == 0 and root.chkoutCtr < 2*self.max_count) or root.chkoutCtr == 0:
+                    self.count += 1
+                self.countAvailTruck(root.right)
+        except Exception as e:
+            print(str(e))
+
+    def searchTreeRecord(self, root):
+        if root is None:
+            return
+        else:
+            self.searchTreeRecord(root.left)
+            if (root.chkoutCtr % 2 == 0 and root.chkoutCtr < 2*self.max_count) or root.chkoutCtr == 0:
+                print(root.UId)
+            self.searchTreeRecord(root.right)
 
     def _availTrucks(self, tNode):
-        self.countAvailTruck(tNode)
-        print(f'{self.count} Vehicle Ids that are currently available to deliver supplies:')
+        # self.countAvailTruck(tNode)
+        print(f'Vehicle Ids that are currently available to deliver supplies:')
         self.searchTreeRecord(tNode)
         print('------------------------------------')
 
+    # mapping tagname with respective functions
     def checkPrompt(self, prompt):
         if prompt is not None:
             print(f'--------- {prompt} -----------')
@@ -355,14 +323,13 @@ class AVLTree:
             if 'highFreqTrucks' in prompt:
                 self.triggerHighFreqTrucks(prompt)
             if 'maxDeliveries' in prompt:
-                self.tmaxDeliveries(prompt)
+                self._maxDeliveries(self.root)
             if 'availTrucks' in prompt:
                 self._availTrucks(self.root)
 
 
 def main():
-    tree = AVLTree()
-
+    myTree = AVLTree()
     def readfile(file):
         """ function to read the input files
         :param file: input files(inputPS2.txt, promptsPS2.txt)
@@ -372,23 +339,28 @@ def main():
             data = f.read().splitlines()
         return data
 
-    # input_file = 'inputPS2.txt'
-    # promp_file = 'promptsPS2.txt'
-    # input_data = readfile(input_file)
-    # truck_ids = list(map(int, input_data))
-    # prompts = readfile(promp_file)
-    truck_ids = [2, 34, 453, 56, 34, 643, 231, 31, 31, 453, 34, 34, 34]
-    for truck in truck_ids:
-        print(truck)
-        tree.triggerReadTruckRec(truck)
+    input_file = "inputPS2.txt"
+    promp_file = "promptsPS2.txt"
+    input_data = readfile(input_file)
+    truck_ids = list(map(int, input_data))
+    prompts = readfile(promp_file)
+    # truck_ids = [1, 34, 453, 56, 34, 643, 231, 31, 31, 453, 34, 34, 34]
+    if len(truck_ids) > 1:
+        for num in truck_ids:
+            myTree.triggerReadTruckRec(num)
+    elif len(truck_ids) == 1:
+        print('Only max Delivery is present')
+    else:
+        print('Tree is empty!')
+    # prompts = ["printTruckRec", "checkTruckRec: 31", "checkTruckRec: 542",
+    #                "printOrderStatus: 11", "highFreqTrucks: 2", "maxDeliveries",
+    #                "availTrucks", "updateTruckRec: 112", "updateTruckRec: 453",
+    #                "printTruckRec", "highFreqTrucks: 10", "maxDeliveries"]
     sys.stdout = open("outputsPS2.txt", "w")
-    prompts = ["printTruckRec", "checkTruckRec: 31", "checkTruckRec: 542",
-               "printOrderStatus: 11", "highFreqTrucks: 2", "maxDeliveries",
-               "availTrucks", "updateTruckRec: 112", "updateTruckRec: 453",
-               "printTruckRec", "highFreqTrucks: 2", "maxDeliveries"]
     for prompt in prompts:
-        tree.checkPrompt(prompt)
+        myTree.checkPrompt(prompt)
     sys.stdout.close()
+
 
 
 if __name__ == '__main__':
